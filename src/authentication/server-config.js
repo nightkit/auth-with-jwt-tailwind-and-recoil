8d266023df510b3e3990ce3ea2049dcd1c0a4a41
Utkarsh Dubey <utkarsh.dubey3jan@gmail.com>
2020-09-10 11:22:46 +0530

    Added the correct env var name.

--- src/authentication/server-config.js
+++ src/authentication/server-config.js
@@ -1,4 +1,4 @@
-export const serverURL = process.env.REACT_SERVER_URL || 'http://localhost:8300';
+export const serverURL = process.env.REACT_APP_SERVER_URL || 'http://localhost:8300';
 
 export const routes = {
     register: "/user",
